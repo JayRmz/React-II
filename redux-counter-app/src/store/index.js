ea9d4a28f8f6c1da594ce32c80d00c8c6ea9385d
Juan Ramirez <ramirezblancasjuanm@gmail.com>
2022-03-14 23:36:08 -0600

    Day 17 Redux basics done

--- redux-counter-app/src/store/index.js
+++ redux-counter-app/src/store/index.js
@@ -1,29 +1,51 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const counterReducer = (state = {
-    counter: 0
-}, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1
-        }
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter-slice';
+import authReducer from './auth-slice'
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer
     }
-
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-        }
-    }
-
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-
-    return state;
-};
-
-const store = createStore(counterReducer);
+});
 
 export default store;
+
+
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+
+//     if (action.type === 'toggle') {
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
+
+//     return state;
+// };
+
+// const store = createStore(counterReducer);
+
+// export default store;
