b4f9aa4397aa8e8d0bc8cfb373e56bd29296256e
Juan Ramirez <ramirezblancasjuanm@gmail.com>
2022-03-15 22:07:47 -0600

    Reduz advanced done

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -1,15 +1,43 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Notification from './components/UI/Notification';
+import { useSelector, useDispatch } from 'react-redux';
+import { sendCartData, fetchCartData } from './store/cart-actions'
+
+let isInital = true;
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification)
+
+  useEffect(() => {
+    dispatch(fetchCartData())
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInital) {
+      isInital = false
+      return
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart))
+    }
+
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
