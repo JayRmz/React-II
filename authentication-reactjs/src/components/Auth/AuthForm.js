def9390604f48fe5a3445e83f28265ca4f974c1d
Juan Ramirez <ramirezblancasjuanm@gmail.com>
2022-03-20 19:13:23 -0600

    Day 24 Auth app done

--- authentication-reactjs/src/components/Auth/AuthForm.js
+++ authentication-reactjs/src/components/Auth/AuthForm.js
@@ -1,12 +1,18 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom'
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const history = useHistory();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false)
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -18,30 +24,48 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     // TODO: Add validation
+    setIsLoading(true);
+    let url;
+
     if (isLogin) {
-
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB8V05hIOPp6eIsvY3tnL5thXPFmfvaHXs'
     } else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB8V05hIOPp6eIsvY3tnL5thXPFmfvaHXs', {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          console.log(response);
-        } else {
-          return response.json().then(data => {
-            console.error(data);
-
-          });
-        }
-      });
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB8V05hIOPp6eIsvY3tnL5thXPFmfvaHXs'
     }
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      setIsLoading(false)
+      if (response.ok) {
+        console.log(response);
+        return response.json();
+
+      } else {
+        return response.json().then(data => {
+          let errorMessage = 'Authentication failed!';
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage)
+        });
+      }
+    }).then(data => {
+      const expirationTime = new Date(new Date().getTime() + (+ data.expiresIn * 1000))
+      authCtx.login(data.idToken, expirationTime.toISOString());
+      history.replace('/');
+    })
+      .catch(error => {
+        alert(error.message);
+      });
 
 
   }
@@ -59,7 +83,8 @@ const AuthForm = () => {
           <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Loading...</p>}
           <button
             type='button'
             className={classes.toggle}
